8b4cc157994bb89d5dc4d50c699e9330a0f887bd
wisdom.eleeza@amalitech.com <wisdom.eleeza@amalitech.com>
2023-05-05 16:29:37 +0000

    react website, understanding useState

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,9 +1,43 @@
-
+import Images from "../images/values.jpg";
+import SectionHead from "./SectionHead";
+import { GiCutDiamond } from "react-icons/gi";
+import { values } from "../data";
+import Card from "../UIcomponents/Card";
 
 const Values = () => {
   return (
-    <div>Values</div>
-  )
-}
+    <section className="values">
+      <div className="container values__container">
+        <div className="values__left">
+          <div className="values__image">
+            <img src={Images} alt="Values Images" />
+          </div>
+        </div>
+        <div className="values__right">
+          <SectionHead icon={<GiCutDiamond />} title="Values"></SectionHead>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+            explicabo atque nulla assumenda! Esse voluptate vel ea est debitis
+            optio commodi, aperiam qui! Hic, totam ea! Quidem molestiae
+            repudiandae delectus?
+          </p>
+          <div className="values__wrapper">
+            {
+                values.map(({id, icon, title, desc}) => {
+                    return (
+                        <Card className='values__value' key={id}>
+                            <span>{icon}</span>
+                            <h4>{title}</h4>
+                            <small>{desc}</small>
+                        </Card>
+                    )
+                })
+            }
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Values
+export default Values;
