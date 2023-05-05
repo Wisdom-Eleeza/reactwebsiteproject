fa2540b27a2f3898cc5feee08d886a8af71a47ca
wisdom.eleeza@amalitech.com <wisdom.eleeza@amalitech.com>
2023-05-05 15:40:51 +0000

    react website, understanding useState

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import MainHeader from '../../components/MainHeader'
 import Programs from '../../components/Programs'
 import './home.css'
+import Values from '../../components/Values'
 
 const Home = () => {
   return (
     <>
     <MainHeader />
     <Programs />
+    <Values />
     </>
   )
 }
